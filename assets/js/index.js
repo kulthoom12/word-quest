6ffda6bab6856ee4bf59e6afa472429086d03c34
kulthoom12 <mussah.kulsum@gmail.com>
2025-06-04 18:13:29 +0100

    // find html by their IDs so we can use them in the game to show, change, or get information from them.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,4 @@
+// words that will be used in the game when playable.
 const words = [
   "sloth",
   "pancake",
@@ -20,5 +21,23 @@ let timer;
 let timeLeft = 60;
 let score = 0;
 let hintsUsed = 0; 
+
+// find html by their IDs so we can use them in the game to show, change, or get information from them.
+
+const startBtn = document.getElementById("start-btn");
+const instructionsBtn = document.getElementById("instructions-btn");
+const instructionsDiv = document.getElementById("instructions");
+const gameContainer = document.getElementById("game-container");
+const startContainer = document.getElementById("start-container");
+const scrambled = document.getElementById("scrambled");
+const word = document.getElementById("word");
+const input = document.getElementById("guess-input");
+const submitBtn = document.getElementById("submit-btn");
+const hintBtn = document.getElementById("hint-btn");
+const restartBtn = document.getElementById("restart-btn");
+const message = document.getElementById("message");
+const scoreDisplay = document.getElementById("score");
+const timerDisplay = document.getElementById("timer");
+
 
 
