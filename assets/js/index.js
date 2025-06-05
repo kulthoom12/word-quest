1676ebb43bd64319ad6166a736a207c4970b0dfb
kulthoom12 <mussah.kulsum@gmail.com>
2025-06-05 10:24:42 +0100

    Add function to shuffle words while playing the game.

--- assets/js/index.js
+++ assets/js/index.js
@@ -40,6 +40,17 @@ const scoreDisplay = document.getElementById("score");
 const timerDisplay = document.getElementById("timer");
 
 
+function shuffleWord (word) {
+let array = word.split('');
+for (let i = array.length - 1; i> 0; i --) {
+  let j = Math.floor(Math.random ()* i); 
+  let temp = array [i];
+  array[i];
+  array[j];
+  array [j] = temp; 
+}
 
+return array;
+}
 
 
