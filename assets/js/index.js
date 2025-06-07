c0f385d1b18cab6d10789a292a1f6684f4f41f88
kulthoom12 <mussah.kulsum@gmail.com>
2025-06-07 08:41:25 +0100

    Display scrambled letters clearly and reveal correct guesses in their positions

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,18 @@
 // words that will be used in the game when playable.
 const words = [
-  "sloth",
-  "pancake",
-  "dragon",
-  "zombie",
-  "robot",
-  "glitter",
-  "ninja",
-  "donut",
-  "wizard",
-  "volcano"
+	"sloth",
+	"pancake",
+	"dragon",
+	"zombie",
+	"robot",
+	"glitter",
+	"ninja",
+	"donut",
+	"wizard",
+	"volcano",
 ];
 
-// important vairables durig the game. 
+// important vairables durig the game.
 
 let currentWord = "";
 let scrambledLetters = [];
@@ -20,7 +20,7 @@ let revealedLetters = [];
 let timer;
 let timeLeft = 60;
 let score = 0;
-let hintsUsed = 0; 
+let hintsUsed = 0;
 
 // find html by their IDs so we can use them in the game to show, change, or get information from them.
 
@@ -39,17 +39,34 @@ const message = document.getElementById("message");
 const scoreDisplay = document.getElementById("score");
 const timerDisplay = document.getElementById("timer");
 
+// This is to shuffle the each word.
 
-function shuffleWord (word) {
-let array = word.split('');
-for (let i = array.length - 1; i> 0; i --) {
-  let j = Math.floor(Math.random ()* i); 
-  let temp = array [i];
-  array[i];
-  array[j];
-  array [j] = temp; 
+function shuffleWord(word) {
+	let array = word.split("");
+	for (let i = array.length - 1; i > 0; i--) {
+		let j = Math.floor(Math.random() * i);
+		let temp = array[i];
+		array[i];
+		array[j];
+		array[j] = temp;
+	}
+	return array;
 }
-return array;
+
+function displayScrambled() {
+	scrambled.textContent = scrambledLetters.join(" ");
 }
 
+// shows where the correct letter is and blank for the others. 
 
+function displayWord() {
+	word.innerHTML = "";
+	for (
+		let i = 0;
+		i < currentWord.length;
+		i++ 
+	)
+		if (revealedLetters[i]) {
+			word.innerHTML += currentWord[i] + " ";
+		} else word.innerHTML += "_ ";
+}
