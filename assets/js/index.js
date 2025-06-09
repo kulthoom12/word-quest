37ec19a51233d6c787dc851d5ec1550b2fa525bb
kulthoom12 <mussah.kulsum@gmail.com>
2025-06-09 12:27:27 +0100

    I tried to mix the letters up but didn’t do it right I forgot to swap both letters, so it didn’t work

--- assets/js/index.js
+++ assets/js/index.js
@@ -40,10 +40,8 @@ function shuffleWord(word) {
 	let array = word.split("");
 	for (let i = array.length - 1; i > 0; i--) {
 		let j = Math.floor(Math.random() * i + 1);
-		let temp = array[i];
-		array[i];
-		array[j];
-		array[j] = temp;
+		array[i]; array[j];
+		[array[j], array[i]];
 	}
 	return array;
 }
