78351663c84212483f6908435cd549a52c446e55
kulthoom12 <mussah.kulsum@gmail.com>
2025-06-11 19:23:36 +0100

    Correct doc string for the js file as what I added was incorrect.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,4 @@
-// words that will be used in the game when playable.
+ /**  Words that will be used in the game when playable. */
 const words = [
 	"sloth",
 	"pancake",
@@ -21,7 +21,7 @@ let timeLeft = 60;
 let score = 0;
 let hintsUsed = 0;
 
-// find html by their IDs so we can use them in the game to show, change, or get information from them.
+// Find html by their IDs so we can use them in the game to show, change, or get information from them.
 const startBtn = document.getElementById("start");
 const instructionsBtn = document.getElementById("instructions-btn");
 const instructionsDiv = document.getElementById("instructions");
@@ -38,7 +38,8 @@ const gameControls = document.getElementById("game-controls");
 const gameContainer = document.getElementById("game");
 const startContainer = document.getElementById("start-container");
 
-// This is to shuffle the each word.
+/** This is to shuffle the each word. */
+
 function shuffleWord(word) {
 	let array = word.split("");
 	for (let i = array.length - 1; i > 0; i--) {
@@ -49,12 +50,12 @@ function shuffleWord(word) {
 	}
 	return array;
 }
-
+/** Display scrambled letters while game is on */
 function displayScrambled() {
 	scrambled.textContent = scrambledLetters.join(" ");
 }
 
-// shows where the correct letter is and blank for the others.
+/** shows where the correct letter is and blank for the others. */
 function displayWord() {
 	word.innerHTML = "";
 	for (let i = 0; i < currentWord.length; i++)
@@ -62,13 +63,13 @@ function displayWord() {
 			word.innerHTML += currentWord[i] + " ";
 		} else word.innerHTML += "_ ";
 }
-// Starts the game
+/** Starts the game */
 
 function startGame() {
 	startContainer.style.display = "none";
 	gameContainer.style.display = "block";
   gameControls.style.display = "block"; 
-  
+
 	message.textContent = "";
 	input.value = "";
 	score = 0;
@@ -104,7 +105,7 @@ function startGame() {
 	}, 1000);
 }
 
-// check guessed word and show hints if needed
+/**  Check guessed word and show hints if needed */
 function checkGuess() {
 	if (input.value != currentWord) {
 		message.textContent = "Wrong! Try again.";
@@ -114,7 +115,7 @@ function checkGuess() {
 		startGame();
 	}
 }
-
+/** Reveals a hint */
 function revealHint() {
 	if (hintsUsed > 3) {
 		message.textContent = "No more hints!";
@@ -136,7 +137,7 @@ function revealHint() {
 	message.textContent = "Hint used: " + hintsUsed;
 }
 
-// starts the game, checks guesses, gives hints, shows instructions, ends or restarts the game
+/** starts the game, checks guesses, gives hints, shows instructions, ends or restarts the game */
 function endGame(msg) {
 	clearInterval(timer);
 	message.textContent = msg;
