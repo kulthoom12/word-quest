967f0bc8ba7e8dfe9a7b2414f15c06aef486714c
kulthoom12 <mussah.kulsum@gmail.com>
2025-06-11 19:56:29 +0100

    Fix shuffleWord to swap letters correctly.

--- assets/js/index.js
+++ assets/js/index.js
@@ -44,9 +44,9 @@ function shuffleWord(word) {
 	let array = word.split("");
 	for (let i = array.length - 1; i > 0; i--) {
 		let j = Math.floor(Math.random() * i + 1);
-		array[i];
-		array[j];
-		[array[j], array[i]];
+		[array[i],
+		array[j]],
+		[array[j], array[i]]
 	}
 	return array;
 }
